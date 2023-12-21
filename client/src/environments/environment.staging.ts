59c9a812986d74ed95d5e4558695b52204ca7d2d
Pierre Nicoli <pierre.nicoli@zenika.com>
2023-12-21 11:47:00 +0100

    Starting to define staging env

---
+++ client/src/environments/environment.staging.ts
@@ -0,0 +1,18 @@
+import { FirebaseOptions } from 'firebase/app';
+
+const firebaseOptions: FirebaseOptions = {
+  apiKey: 'AIzaSyBY-HULaNWkEPCoQWCIy3vOB2KB98z4HGs',
+  authDomain: 'feedzback-v2-dev.firebaseapp.com',
+  projectId: 'feedzback-v2-dev',
+  storageBucket: 'feedzback-v2-dev.appspot.com',
+  messagingSenderId: '195593920321',
+  appId: '1:195593920321:web:e9d6255b8ef9a46466cbc0',
+  measurementId: 'G-29CT4FJL84',
+};
+
+export const environment = {
+  production: false,
+  firebaseOptions,
+  apiBaseUrl: 'https://feedzback-server-ed26lzziyq-ew.a.run.app',
+  allowedEmailDomains: ['zenika.com'],
+};
