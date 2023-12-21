a210aca6f0eb7c865af5e992b2de0dd362edec9a
Stéphane Francel <stephane.francel@gmail.com>
2023-12-22 00:19:53 +0100

    server: apply cors and add PORT env variable

--- server/src/core/config/config.ts
+++ server/src/core/config/config.ts
@@ -4,7 +4,7 @@ import { getAppEnvironment } from './config.utils';
 export const appConfigLoader = (): AppConfig => ({
   appEnv: getAppEnvironment(process.env.NODE_ENV),
 
-  serverPort: 3000,
+  serverPort: Number(process.env.PORT!),
 
   clientUrl: process.env.CLIENT_URL!,
 
